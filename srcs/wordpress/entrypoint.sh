8d5c0a106b5e2dbd65c1bc145e0f70a7ad3a53ce
cubernetes <timo42@proton.me>
2025-10-21 08:21:13 +0200

    Add & Improve wordpress entrypoint/Dockerfile

---
+++ srcs/wordpress/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -eux
+
+wp core download
+wp config create --dbname=test_db --dbhost=tmp-mariadb:3306 --dbuser=test_user --prompt=dbpass<<'EOPASS'
+test_password
+EOPASS
+wp db create
+wp core install --url=localhost --title="WP-CLI" --admin_user=wpcli --admin_password=wpcli --admin_email=info@wp-cli.org
+
+exec "$@"
